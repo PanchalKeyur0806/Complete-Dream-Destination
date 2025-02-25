79a03b7332704ab484dadf6fd601ffb45f341722
PanchalKeyur0806 <panchalkeyur694@outlook.com>
2025-02-25 10:40:56 +0530

    Added styles for /resetpassword/:token route

--- dream-destination/src/component/ResetPassword/ResetPassword.jsx
+++ dream-destination/src/component/ResetPassword/ResetPassword.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import "./ResetPassword.css";
 
 const ResetPassword = () => {
   const [password, setPassword] = useState("");
